9e64d4da7a6cc3a5203b1195ae8ee3ca03606684
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-11-07 18:46:07 +0530

    client side front end rady

--- src/Pages/SymtomsChecker/Components/Home.js
+++ src/Pages/SymtomsChecker/Components/Home.js
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Row, Col, Button } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
+import CHECKER from '../../../assets/cheacker.png'
 
 function Home() {
     return (
         <Row className="symtoms_home mt-2">
+            <Col xl={6} className="mt-2">
+                <img className="img-fluid" src={CHECKER} alt="" />
+            </Col>
             <Col xl={6} className="mt-2">
                 <h3>Hello !</h3>
                 <p>
                     You’re about to use a short (3 min), safe and anonymous health checkup. Your answers will be carefully analyzed and you’ll learn about possible causes of your symptoms.
                 </p>
                 <Link to="/interview"><Button>Next</Button></Link>
-            </Col>
-            <Col xl={4} className="mt-2">
-                
             </Col>
         </Row>
     )
