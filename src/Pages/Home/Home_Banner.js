8cfa3292129396e15ffa2cda1be20e79782e3b04
pruthvi7384 <pruthvirajrajput575@gmail.com>
2021-11-07 12:07:19 +0530

    symtoms cheacker ui created

--- src/Pages/Home/Home_Banner.js
+++ src/Pages/Home/Home_Banner.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { Container, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+
 function HomeBanner() {
     return (
         <Container fluid className="home_page_banner">
             <div className="home_hero_section">
-                <h1><span>Your Health</span> is Our Priority !</h1>
-                <h4>we care about your health.</h4>
-                <Link to="/symptomschecker"><Button>Check Your Symptoms Now</Button></Link>
+                    <h1><span>Your Health</span> is Our Priority !</h1>
+                    <h4>we care about your health.</h4>
+                    <Link to="/symptomschecker"><Button>Check Your Symptoms Now</Button></Link>
             </div>
         </Container>
     )
